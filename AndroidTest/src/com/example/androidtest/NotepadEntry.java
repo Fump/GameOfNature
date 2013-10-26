27a5b0438c8f8a03c79a8f230055b32b3e678b44
Fump <carl.pontus.hagberg@gmail.com>
2013-10-26 23:44:09 +0200

    Tog bort parcel

--- AndroidTest/src/com/example/androidtest/NotepadEntry.java
+++ AndroidTest/src/com/example/androidtest/NotepadEntry.java
@@ -3,21 +3,9 @@ package com.example.androidtest;
 import android.os.Parcel;
 import android.os.Parcelable;
 
-public class NotepadEntry implements Parcelable  {
+public class NotepadEntry  {
 	private String title;
 	private String text;
-	
-	public static final Parcelable.Creator<NotepadEntry> CREATOR
-    = new Parcelable.Creator<NotepadEntry>() {
-
-		public NotepadEntry createFromParcel(Parcel in) {
-		  return new NotepadEntry(in);
-		}
-
-		public NotepadEntry[] newArray(int size) {
-		  return new NotepadEntry[size];
-		}
-	};
 	
 	public NotepadEntry(Parcel source) {
 	    title = source.readString();
@@ -48,16 +36,5 @@ public class NotepadEntry implements Parcelable  {
 	@Override
 	public String toString() {
 		return title;
-	}
-
-	@Override
-	public int describeContents() {
-		return 0;
-	}
-
-	@Override
-	public void writeToParcel(Parcel dest, int flags) {
-		dest.writeString(title);
-		dest.writeString(text);
 	}
 }
