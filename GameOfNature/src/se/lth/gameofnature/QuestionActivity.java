1ae1dac3d1b2429e683c67c8bbcd10302497d77c
Sparf <Sparf@Sparf-Dator>
2013-11-07 13:24:14 +0100

    Frågeskärm

---
+++ GameOfNature/src/se/lth/gameofnature/QuestionActivity.java
@@ -0,0 +1,14 @@
+package se.lth.gameofnature;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class QuestionActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_question);
+	}
+}
