d49d2f9580d2fa23b09a165f3bbd44adc807d578
AlleBalle <dat11anh@student.lu.se>
2013-11-10 22:17:34 +0100

    Skapat statisitkskärm,knapp från statistikskärm till spelplan, lagt till nya preliminära färger

--- GameOfNature/src/se/lth/gameofnature/Alternativsida.java
+++ GameOfNature/src/se/lth/gameofnature/Alternativsida.java
@@ -5,12 +5,11 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 public class Alternativsida extends Activity {
-	private ImageButton currColor;
+	private ImageButton currentColor;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -18,18 +17,18 @@ public class Alternativsida extends Activity {
 		setContentView(R.layout.activity_alternativsida);
 		//Förvald färg vid start
 		LinearLayout paintLayout = (LinearLayout)findViewById(R.id.choose_colors);
-		currColor = (ImageButton)paintLayout.getChildAt(0);
-		currColor.setImageDrawable(getResources().getDrawable(R.drawable.colorpressed));
+		currentColor = (ImageButton)paintLayout.getChildAt(0);
+		currentColor.setImageDrawable(getResources().getDrawable(R.drawable.colorpressed));
 	}
 	
 	//Lyssna på knapptryck
 	public void colorClicked(View view){
-		if(view!=currColor){
-			ImageButton imgView = (ImageButton)view;
+		if(view!=currentColor){
+			ImageButton newColor = (ImageButton)view;
 			//ändra backgrundsfärgen vid tryck så att man se vilken färg man har valt
-			imgView.setImageDrawable(getResources().getDrawable(R.drawable.colorpressed));
-			currColor.setImageDrawable(getResources().getDrawable(R.drawable.colorbackgroundproperties));
-			currColor=(ImageButton)view;
+			newColor.setImageDrawable(getResources().getDrawable(R.drawable.colorpressed));
+			currentColor.setImageDrawable(getResources().getDrawable(R.drawable.colorbackgroundproperties));
+			currentColor=newColor;
 			}	
 	}
 	
