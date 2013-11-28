d31773877ccd2ed32deed23379af5a735c56437c
Fump <carl.pontus.hagberg@gmail.com>
2013-11-28 21:21:18 +0100

    Fixade röd kod i FinalQuestion.

--- GameOfNature/src/se/lth/gameofnature/questions/FinalQuestion.java
+++ GameOfNature/src/se/lth/gameofnature/questions/FinalQuestion.java
@@ -1,22 +1,20 @@
 package se.lth.gameofnature.questions;
 
 import se.lth.gameofnature.QuestionActivity;
-import se.lth.gameofnature.StatisticsActivity;
+import se.lth.gameofnature.WinnerActivity;
 import se.lth.gameofnature.gamemap.markers.TaskMarker;
 import android.content.Context;
 import android.content.Intent;
 
 public class FinalQuestion extends Question{
-	
-	
 	public FinalQuestion(String id, String questionTxt,
 			String[] answers, int correctAnswer) {
-		super(id, Question.QUESTION_TYPE_TEXT, questionTxt, answers, correctAnswer);
+		super(id, Question.QUESTION_TYPE_FINAL, questionTxt, answers, correctAnswer);
 	}
 	
 	@Override
 	public void startQuestionActivity(Context source, String taskMarkerId) {
-	Intent intent = new Intent (source,StatisticsActivity.class);
-	source.startActivity(intent);
+		Intent intent = new Intent (source, WinnerActivity.class);
+		source.startActivity(intent);
 	}
 }
